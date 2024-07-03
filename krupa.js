e30c4ec4a700041852648329b7cfe6c9074825f8
Krups24 <krupasharma24@gmail.com>
2024-07-03 22:44:07 +0530

    Add files via upload

---
+++ krupa.js
@@ -0,0 +1,83 @@
+// const pi = 3.14;
+// let person = "John Doe";
+// let answer = 'Yes I am!';
+
+// document.getElementById("demo").innerHTML =
+// pi + "<br>" + person + "<br>" + answer;
+
+//  let carName;
+//  document.getElementById("demo").innerHTML = carName;
+ 
+
+// function myfunction (a,b) {
+//     return a * b;
+// }
+
+// let x = myFunction(4, 3);
+// let y = mydivision(3,2);
+// let z = myadd(2,7);
+// let w = mysubb(8,3);
+
+// document.getElementById("demo").innerHTML = z;
+//  function myFunction(a, b) {
+//   return a * b;
+// }
+
+//  document.getElementById("demo1").innerHTML = y;
+//   function mydivision(a, b) {
+//     return a / b;
+//  }
+
+//  document.getElementById("demo2").innerHTML = z;
+//  function myadd(a, b) {
+//      return a + b;
+// }
+
+// document.getElementById("demo3").innerHTML = w;
+// function mysubb(a,b) {
+//     return a-b ;
+// }
+
+//   function myFunction() {
+//       document.getElementById("demo4").innerHTML = "krupa";
+//        }
+// function myfunction() {
+    //     document.getElementById("1").style.color = "red";
+    // }
+
+    //    function myFunction() {
+    //     document.getElementById("field2").value = document.getElementById("field1").value;
+    //   }
+
+
+    // function myFunction() {
+    //     document.getElementById("demo").innerHTML="krupa"
+    // }
+
+//  function mouseDown () {
+//     document.getElementById("p1").style.color = "pink";
+//  }
+
+//  function mouseUp () {
+//     document.getElementById("p1").style.color = "black";
+//  }
+
+// function bigImg(x) {
+//     x.style.height = "64px";
+//     x.style.width = "64px";
+//   }
+//   function normalImg(x) {
+//     x.style.height = "29px";
+//     x.style.width = "29px";
+//   }  
+
+function myFunction() {
+  document.body.style.backgroundColor="pink";
+}
+
+ function myImage() {
+   document.body.style.backgroundColor="pink";
+   document.body.style.backgroundImage="url('background.jpg')";
+}
+
+
